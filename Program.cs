2367c54d49b711373d1ec6764b0e5a317e9cd7e7
Bhuvi Sethuraman <Bhuvi.Sethuraman@softwire.com>
2021-09-20 11:35:34 +0100

    Part 4: Added changes

--- Program.cs
+++ Program.cs
@@ -6,12 +6,21 @@ namespace FizzBuzz_cSharp
     {
         static void Main(string[] args)
         {
+            string[] names = {"Fizz", "Buzz"};
+            
             for(int i = 1; i <= 100; i++)
             {
+                var output = "";
+
                 if(i % 3 == 0)
-                    Console.WriteLine("Fizz");
-                else
-                    Console.WriteLine(i);
+                    output += names[0];
+                if(i % 5 == 0)
+                    output += names[1];
+                
+                if(string.IsNullOrEmpty(output))
+                    output += i;
+                
+                Console.WriteLine(output);
             }
         }
     }
