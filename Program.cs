a4c4cabf74a37bbab14dad8146cc6761b553242f
Bhuvi Sethuraman <Bhuvi.Sethuraman@softwire.com>
2021-09-20 14:30:26 +0100

    Stretch goals - used static methods

--- Program.cs
+++ Program.cs
@@ -4,23 +4,54 @@ namespace FizzBuzz_cSharp
 {
     class Program
     {
+        //static string[] names = {"Fizz", "Buzz"};
+        static string FizzRule(int num, string output)
+        {
+            return((num % 3 == 0) ? output += "Fizz": output);
+        }   
+
+        static string BuzzRule(int num, string output)
+        {
+            return((num % 5 == 0) ? output += "Buzz" : output);
+        }
+
+        static string BangRule(int num, string output)
+        {
+            return((num % 7 == 0) ? output += "Bang" : output);
+        }
+
+        static string BongRule(int num, string output)
+        {
+            return((num % 11 == 0) ? output = "Bong" : output);
+        }
+
+        static string FezzRule(int num, string output)
+        {
+            if(num % 13 == 0)
+            {
+                var pos = output.IndexOf('B');
+                return ((pos > 0) ? (output.Substring(0, pos) + "Fezz" + output.Substring(pos)) : ("Fezz" + output)); 
+            }
+            return output;
+        }
+
         static void Main(string[] args)
         {
-            string[] names = {"Fizz", "Buzz"};
             
-            for(int i = 1; i <= 100; i++)
+            for(int i = 1; i <= 195; i++)
             {
                 var output = "";
 
-                if(i % 3 == 0)
-                    output += names[0];
-                if(i % 5 == 0)
-                    output += names[1];
+                output = FizzRule(i, output);
+                output = BuzzRule(i, output);
+                output = BangRule(i, output);
+                output = BongRule(i, output);
+                output = FezzRule(i, output);
                 
-                if(string.IsNullOrEmpty(output))
-                    output += i;
+                //if(string.IsNullOrEmpty(output))
+                    //output += i;
                 
-                Console.WriteLine(output);
+                Console.WriteLine((output == "")? i : output);
             }
         }
     }
