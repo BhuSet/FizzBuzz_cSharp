424e4095444cd1ce115f86cdb14a0f6d11c05ec7
Bhuvi Sethuraman <Bhuvi.Sethuraman@softwire.com>
2021-09-20 11:13:20 +0100

    Part 3: Print Fizz for numbers that are multiple by 3

--- Program.cs
+++ Program.cs
@@ -7,8 +7,12 @@ namespace FizzBuzz_cSharp
         static void Main(string[] args)
         {
             for(int i = 1; i <= 100; i++)
-
-                Console.WriteLine(i);
+            {
+                if(i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
         }
     }
 }
